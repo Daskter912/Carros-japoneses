fac7ab90a47473b1b6db6b41bfddbaa197cbe95e
Dask <leminitomas912@gmial.com>
2025-01-14 03:00:06 -0600

    commit de prueba

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ListaAutos from './helpers/ListaAutos.json'
 
 
-
-
-
 function ElementoLista(props) {
     return(
         <li>
@@ -16,7 +13,8 @@ function ElementoLista(props) {
 export default function Header() {
   return (
     <div>
-        <h1>Renderizado de Autos</h1>
+      <h1>Autos japoneses</h1>
+        <h2>Renderizado de Autos</h2>
 
         <h3>Carros japoneses</h3>
         <ul>
