71c3488c72de582c0cd1c5520b65dc47075d2f00
Dask <leminitomas912@gmial.com>
2025-01-17 14:09:43 -0600

    cambios en el header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../Componentes/Header'
+import Aside from '../Componentes/Aside'
 
 export default function Home() {
   return (
    <>
    <Header></Header>
+   <Aside></Aside>
+
    
    </>
   )
