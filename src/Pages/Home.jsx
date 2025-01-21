9bac304db022e3cc02afea002ec815810673f34e
Dask <leminitomas912@gmial.com>
2025-01-20 20:51:53 -0600

    se reestruturo el header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Header from '../Componentes/Header'
-import Draw from '../Componentes/Draw'
 
 
 export default function Home() {
   return (
       <>
       <Header></Header>
-      <Draw></Draw>
       </>
   )
 }
