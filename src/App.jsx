06634a6bf9a4cc88f1ca9d5ca8990c7ef1cb8ae1
Dask <leminitomas912@gmial.com>
2025-02-25 02:38:50 -0600

    cambio de ubicacion de archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
+import React from 'react';
 
 
 function App() {
 
   return (
     <>
-    <Home></Home>
+        <Home></Home>
+
+
     </>
-  )
+    
+    
+  );
 }
 
 export default App
